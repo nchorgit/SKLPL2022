cced6909fd5b758a38c3ac6645f7d8fa627893d2
Nickos Chortis <66328362+nchorgit@users.noreply.github.com>
2022-04-14 16:41:03 +0300

    U

---
+++ favicon.js
@@ -0,0 +1,8 @@
+function setFavicons(favImg){
+    let headTitle = document.querySelector('head');
+    let setFavicon = document.createElement('link');
+    setFavicon.setAttribute('rel','shortcut icon');
+    setFavicon.setAttribute('href',favImg);
+    headTitle.appendChild(setFavicon);
+}
+setFavicons('https://sites.google.com/site/sklplfiles/files/sklico.png');
